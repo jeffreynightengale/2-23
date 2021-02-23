46656aebbea2c822bb4a91248d9b5d4d31eda856
Jeff <Jeff@DESKTOP-DFBAMBV>
2021-02-23 17:54:43 -0600

    Loops for 2/23

---
+++ 2-23/2-23/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _2_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Count all the way up to 100
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine($"{i}") ;
+            }
+
+            for (int i = 3; i < 1001; i += 3)
+            {
+                //if (i % 3 == 0)
+                //{
+                    Console.WriteLine(i);
+                //}
+            }
+
+            Console.WriteLine();
+            for (int i = 10; i >= 0; i--)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine("BLAST OFF");
+                }
+                else
+                {
+                Console.WriteLine(i);
+                }
+               
+            }
+        }
+    }
+}
