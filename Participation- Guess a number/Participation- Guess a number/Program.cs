734bde381c994b8c99ae66ed6e6fff22714ae798
Jeff <Jeff@DESKTOP-DFBAMBV>
2021-02-23 23:46:48 -0600

    started participation called Guess a number

---
+++ Participation- Guess a number/Participation- Guess a number/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Participation__Guess_a_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int randomMin;
+            Random randMin = new Random();
+            randomMin = randMin.Next(1, 6); //I want a random minimum for them to guess from 1 to 5
+            
+            Console.WriteLine($"Guess a random minimum number. (1-5) >>");
+            string answer = Console.ReadLine();
+
+            while (int.TryParse(answer, out randomMin) == false)
+            {
+                Console.WriteLine("Your random guess was incorrect.");
+                answer = Console.ReadLine();
+            }
+
+
+            Random randMax = new Random();
+            int randomMax = randMax.Next(6, 11); //I want a random max for them to guess from 6 to 10
+        }
+    }
+}
