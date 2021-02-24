e32f080a10c9e55409e8a8086f49eccef46623d6
Jeff <Jeff@DESKTOP-DFBAMBV>
2021-02-23 18:01:39 -0600

    Did a while statement

---
+++ 2-23 anotha/2-23 anotha/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _2_23_anotha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your lucky # >>");
+            string answer = Console.ReadLine();
+            int luckyNum;
+
+            while (int.TryParse(answer, out luckyNum) == false)
+            {
+                Console.WriteLine("Sorry you entered an invalid integer. Please enter another number");
+                answer = Console.ReadLine();
+            }
+
+            Console.WriteLine("Good job. you got it right");
+        }
+    }
+}
